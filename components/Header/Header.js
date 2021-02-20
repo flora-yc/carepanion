37b8cedcfb8322ea06c57ee4a0bf57cc73715137
chenselena <selenachenyz@gmail.com>
2021-02-20 12:22:43 -0800

    added todays date header

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { View, Image, Text, StyleSheet } from "react-native";
 import AppLoading from "expo-app-loading";
+import moment from "moment";
 import { useFonts } from "expo-font";
 
 function Header() {
+  var today = moment().format("MMMM Do, yyyy");
   let [fontsLoaded] = useFonts({
-    RobotoMono: require("../../assets/fonts/RobotoMono.ttf"),
+    RobotoMono: require("../../assets/fonts/RobotoMono-Bold.ttf"),
   });
   if (!fontsLoaded) {
     return <AppLoading />;
@@ -13,7 +15,9 @@ function Header() {
     return (
       <View style={styles.container}>
         <Image source={require("../../assets/headerLarger.png")} />
-        <Text style={styles.textNotDate}>Hello</Text>
+        <Text style={styles.textNotDate}>Today is</Text>
+        <Text style={styles.textDate}>{today}</Text>
+        <Text style={styles.textMotivation}>You're doing great!</Text>
       </View>
     );
   }
@@ -27,8 +31,26 @@ const styles = StyleSheet.create({
     color: "#9492D9",
     fontFamily: "RobotoMono",
     position: "absolute",
-    zIndex: 2,
     fontSize: 30,
+    bottom: 140,
+    left: 40,
+  },
+  textDate: {
+    color: "#7271C1",
+    fontFamily: "RobotoMono",
+    position: "absolute",
+    fontSize: 30,
+    bottom: 75,
+    left: 40,
+    width: "70%",
+  },
+  textMotivation: {
+    color: "#47476D",
+    fontFamily: "RobotoMono",
+    position: "absolute",
+    fontSize: 16,
+    bottom: 55,
+    left: 40,
   },
 });
 
