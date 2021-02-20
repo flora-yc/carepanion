c8e9da54dd758e6e581bbe4adb070635337471a8
anson1014 <anson1014@hotmail.com>
2021-02-20 01:35:57 -0800

    added image icon

---
+++ components/CategoryIcon.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Image, StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  logo: {
+    width: 60,
+    height: 60,
+  },
+});
+
+const CategoryIcon = () => {
+    return (
+        <View>
+            <Image
+                styles={styles.logo}
+                source={{
+                    uri: `../assets/${props.src}`
+                }}
+            /> 
+        </View>
+    )
+}
+
+export default CategoryIcon;
--- screens/Homepage/Homepage.js
+++ screens/Homepage/Homepage.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import CategoryIcon from '../../components/CategoryIcon'
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function Homepage() {
   return (
     <View style={styles.container}>
       <Text>Open up Homepage.js to start working on your app!</Text>
+      <CategoryIcon src="sleepIcon.png" />
+      <CategoryIcon src="breakIcon.png" />
+      <CategoryIcon src="generalIcon.png" />
+      <CategoryIcon src="medication.png" />
+      <CategoryIcon src="negativity.png" />
     </View>
   );
 }
