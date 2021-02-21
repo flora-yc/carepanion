6bb0ae8425482611fbfe40ceca4db2025ebdb5bf
flora chen <flora1591@hotmail.com>
2021-02-20 21:56:07 -0800

    updated calendar buttons, and finished calendar screen

--- components/Component/CalendarDate.js
+++ components/Component/CalendarDate.js
@@ -3,27 +3,27 @@ import { StyleSheet, Text, View } from "react-native";
 
 export function CalendarDate(props) {
     return ( 
-    <View style={{
-        width: 53,
-        height: 53,
-        justifyContent: "center",
-        borderRadius: 53 / 2,
-        borderWidth: 2,
-        borderColor: '#D4DCFA',
-      }}>
-        <Text style={{
-          alignSelf: 'center',
-          fontWeight: 'bold',
-          color: '#50506A',
-          fontSize: 15,
-        }}>{props.dayOfTheWeek}</Text>
-        <Text style={{
-          alignSelf: 'center',
-          fontWeight: 'bold',
-          color: '#50506A',
-          fontSize: 15,
-        }}>{props.number}</Text>
-      </View>
+      <View style={{
+          width: 48,
+          height: 48,
+          justifyContent: "center",
+          borderRadius: 45 / 2,
+          borderWidth: 2,
+          borderColor: '#D4DCFA',
+        }}>
+          <Text style={{
+            alignSelf: 'center',
+            fontWeight: 'bold',
+            color: '#50506A',
+            fontSize: 15,
+          }}>{props.dayOfTheWeek}</Text>
+          <Text style={{
+            alignSelf: 'center',
+            fontWeight: 'bold',
+            color: '#50506A',
+            fontSize: 15,
+          }}>{props.number}</Text>
+        </View>
     );
 };
 
@@ -32,10 +32,10 @@ export function CalendarDateToday(props) {
 
     return ( 
     <View style={{
-        width: 53,
-        height: 53,
+        width: 48,
+        height: 48,
         justifyContent: "center",
-        borderRadius: 53 / 2,
+        borderRadius: 45 / 2,
         backgroundColor: '#D4DCFA'
       }}>
         <Text style={{
--- screens/CalendarScreen/CalendarScreen.js
+++ screens/CalendarScreen/CalendarScreen.js
@@ -8,13 +8,42 @@ import {CalendarDate, CalendarDateToday} from "../../components/Component/Calend
 import {CalendarPopup} from "../../components/Component/CalendarPopup.js";
 import { AntDesign } from '@expo/vector-icons'; 
 import Modal from 'react-native-modal';
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
 
 export default function CalendarScreen() {
+
+  const [actionTriggered, setActionTriggered] = useState(''); 
+  const [isModalVisible, setModalVisible] = useState(false);
+  
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+
     return(
         <View style={styles.container}>
             <Header />
-            
-        </View>
+            <AntDesign style = {{bottom: 30}}name="calendar" size={60} color="#7271C1" />
+            <Calendar onDayPress={() => {
+        setActionTriggered('ACTION_1');
+        setModalVisible(true);
+        toggleModal}}
+  backgroundColor= "#E5E5E5"
+  calendarBackground = '#E5E5E5'
+  arrowColor = '#E5E5E5'
+/>
+         <Modal isVisible={isModalVisible} style = {{top: 300}}>
+
+         {actionTriggered === 'ACTION_1' ?
+             <View style={{flex: 1, alignItems: 'center', position: 'center'}}>
+               <CalendarPopup date="Hi"/>
+               <Button title="Hide" onPress={toggleModal}  color="#fff" style={styles.button}/>
+             </View> : null}
+      </Modal>
+      <TouchableOpacity style = {{  position: "absolute", top: 40, left: 25}}>
+      <AntDesign name="arrowleft" size={25} color="#9492D9" /> 
+        </TouchableOpacity>
+      </View>
+
     );
 
 }
@@ -23,7 +52,7 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: "#F6F7FF",
-      alignItems: 'stretch'
+      alignItems: 'center',
       // alignItems: "center",
       // justifyContent: "center",
     },
